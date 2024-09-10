b3c71ac31951a5292cc88b24251f2f768a5261f9
Ibrahem <153446257+iibrahemali@users.noreply.github.com>
2024-09-10 12:42:54 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-//App.js 
+//App.js
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Cookies from 'js-cookie';
