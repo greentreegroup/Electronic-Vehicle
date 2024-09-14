f8870b8106c6411fb8a63378532f0c8414c02735
greentreegroup <86478969+greentreegroup@users.noreply.github.com>
2024-09-14 14:54:22 -0700

    Update Chatbot.js

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -44,7 +44,7 @@ const Chatbot = () => {
       </button>
       {!isMinimized && (
         <iframe
-          src="https://copilotstudio.microsoft.com/environments/Default-6ffa26de-1846-4087-81e9-2a9ec717152f/bots/cr419_chatbot/webchat?__version__=2"
+          src="https://copilotstudio.microsoft.com/environments/Default-6ffa26de-1846-4087-81e9-2a9ec717152f/bots/cr419_evCopilot/webchat?__version__=2"
           frameBorder="0"
           className="chatbot-iframe"
           title="Chatbot"
