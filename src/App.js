6c5be062046c650901128d52428d6274e0f34bae
sktrinh12 <sktrinh12@gmail.com>
2024-09-30 10:34:05 -0500

    Spencer Trinh, IT, change vertical spacing for navbar, add cart route, auto-format, add provider for cart

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 //App.js
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Cookies from 'js-cookie';
-import Sidebar from './components/Sidebar';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Cookies from "js-cookie";
+import Sidebar from "./components/Sidebar";
 
-import TrackRecord from './TrackRecord';
-import InvestmentListings from './components/InvestmentListings';
-import InvestmentDetails from './components/InvestmentDetails';
-import UserProfile from './components/UserProfile';
-import HomePage from './HomePage';
-import HelpCenter2 from './HelpCenter2';
+import TrackRecord from "./TrackRecord";
+import InvestmentListings from "./components/InvestmentListings";
+import InvestmentDetails from "./components/InvestmentDetails";
+import UserProfile from "./components/UserProfile";
+import HomePage from "./HomePage";
+import HelpCenter2 from "./HelpCenter2";
 
-import './App.css';
-import Chatbot from './Chatbot';
-import ResearchPage from './components/ResearchPage';
+import "./App.css";
+import Chatbot from "./Chatbot";
+import ResearchPage from "./components/ResearchPage";
 //import ContactForm from './components/ContactForm';
 //import AboutUsServiceInfo from './components/AboutUs-ServiceInfo';
-import CarSearch from './components/CarSearch';
-import CarSearchDetails from './components/CarSearchDetails';
-import FuelSavingCalc from './components/FuelSavingCalc';
+import CarSearch from "./components/CarSearchCartCompts/CarSearch";
+import CarDetails from "./components/CarSearchCartCompts/CarDetails";
+import Cart from "./components/CarSearchCartCompts/ShoppingCartPage";
+import FuelSavingCalc from "./components/FuelSavingCalc";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const loggedInStatus = Cookies.get('isLoggedIn');
-    if (loggedInStatus === 'true') {
+    const loggedInStatus = Cookies.get("isLoggedIn");
+    if (loggedInStatus === "true") {
       setIsLoggedIn(true);
     }
     console.log(`isLoggedIn: ${isLoggedIn}`);
@@ -40,22 +41,41 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/HelpCenter2" element={<HelpCenter2 />} />
             <Route path="/track-record" element={<TrackRecord />} />
-            <Route path="/listings" element={<InvestmentListings minPrice={-1} maxPrice={-1} sortByRecent={false} propertyType={'none'} />} />
-            <Route path="/listings/:listingId" element={<InvestmentDetails />} /> {/* Route for listing details page */}
-            <Route path="/userProfile/:userId" element={<UserProfile />} /> {/* Route for listing details page */}
+            <Route
+              path="/listings"
+              element={
+                <InvestmentListings
+                  minPrice={-1}
+                  maxPrice={-1}
+                  sortByRecent={false}
+                  propertyType={"none"}
+                />
+              }
+            />
+            <Route
+              path="/listings/:listingId"
+              element={<InvestmentDetails />}
+            />{" "}
+            {/* Route for listing details page */}
+            <Route path="/userProfile/:userId" element={<UserProfile />} />{" "}
+            {/* Route for listing details page */}
             <Route path="/Research" element={<ResearchPage />} />
             {/*
             <Route path="/Parts" element={<FeaturedParts />} />  Route for listing details page
             <Route path="/Cars" element={<FeatureCars />} />  Route for listing details page
             */}
-            <Route path="/CarSearch" element={<CarSearch />} /> {/* Route for searching/filtering electric vehicles database */}
-            <Route path="/car-details/:id" element={<CarSearchDetails />} />
-            <Route path="/Research" element={<ResearchPage />} /> {/* Route for listing details page */}
+            <Route path="/CarSearch" element={<CarSearch />} />{" "}
+            {/* Route for searching/filtering electric vehicles database */}
+            <Route path="/car-details/:id" element={<CarDetails />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/Research" element={<ResearchPage />} />{" "}
+            {/* Route for listing details page */}
             {/*
             <Route path="/Contact" element={<ContactForm />} />  Route for listing details page
             <Route path="/AboutUs" element={<AboutUsServiceInfo />} /> Route for listing details page
             Other routes */}
-            <Route path="/FuelSavingCalc" element={<FuelSavingCalc />} /> {/* Route to fuel savings calculator */}
+            <Route path="/FuelSavingCalc" element={<FuelSavingCalc />} />{" "}
+            {/* Route to fuel savings calculator */}
           </Routes>
         </main>
         <Chatbot />
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { CarProvider } from "./components/CarSearchContext";
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { CarProvider } from "./components/CarSearchCartCompts/CarContext";
+import { CartProvider } from "./components/CarSearchCartCompts/CartContext";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <CarProvider>
-    <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </CarProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
