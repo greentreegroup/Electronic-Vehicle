363c8a95adb9cf01d28de67b798f815030bd33ff
MaddyPal <madhavipal2314@gmail.com>
2024-10-09 20:03:40 -0500

    Madhavi, IT, updated the homepage using baoli as refernece

---
+++ src/components/BlogSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './BlogSection.css';
+
+const BlogCard = ({ title, description }) => {
+  return (
+    <div className="blog-card">
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <div className="button-container">
+      <button> <a href="/">Get Started</a></button>
+      </div>
+    </div>
+  );
+};
+const BlogSection = () => {
+  return (
+    <section className="blog-section">
+      <h1>Latest Blog Posts</h1>
+      <div className="cards-container">
+        <BlogCard 
+          title="Are You Looking For a China Electric Car ?" 
+          description="We are committed to providing our customers with exceptional service." 
+        />
+        <BlogCard 
+          title="Are You Looking For a China Hybrid Cars ?" 
+          description="We are committed to providing our customers with exceptional service." 
+        />
+      </div>
+    </section>
+  );
+};
+
+export default BlogSection;
---
+++ src/components/PremiumBrands.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import './PremiumBrands.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function PremiumBrands() {
+  return (
+    <div className="premium-brands-container">
+      <h2>Our Premium Brands</h2>
+      <div className="brand-logos row">
+        <div className="col-6 col-md-2 brand-item">
+          <a href="/">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/0/0e/BYD_Auto_Logo.svg" alt="Brand 1" />
+            </a> <p>BYD</p>
+        
+        </div>
+        <div className="col-6 col-md-2 brand-item">
+          <a href="/">
+            <img src="https://e7.pngegg.com/pngimages/510/529/png-clipart-car-logo-roewe-icons-logos-emojis-car-logos.png" alt="Brand 2" />
+            </a> <p>Roewe</p>
+         
+        </div>
+        <div className="col-6 col-md-2 brand-item">
+          <a href="/">
+            <img src="https://i.pinimg.com/736x/58/bc/c8/58bcc81b0825551e87e289f4263c7feb.jpg" alt="Brand 3" />
+            </a> <p>MG</p>
+          
+        </div>
+        <div className="col-6 col-md-2 brand-item">
+          <a href="/">
+            <img src="https://guangcaiauto.com/wp-content/uploads/elementor/thumbs/IM-logo-qhp2w7ktcry1mjbmenjprk72cvsg2xlwj4v22wv9i4.png" alt="Brand 4" />
+            </a>  <p>IM Motor</p>
+          
+        </div>
+        <div className="col-6 col-md-2 brand-item">
+          <a href="/">
+            <img src="https://1000logos.net/wp-content/uploads/2023/09/Nio-Logo.png" alt="Brand 5" />
+            </a><p>NIO</p>
+        
+        </div>
+        <div className="col-6 col-md-2 brand-item">
+          <a href="/">
+            <img src="https://static.wixstatic.com/media/f2bf43_a50c0080788f4b9fa5bff97c40bbdc1f~mv2.png" alt="Brand 6" />
+            </a> <p>GAC Motor</p>
+         
+        </div>
+        <div className="col-6 col-md-2 brand-item">
+          <a href="/">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/21/Li_Auto_logo.png" alt="Brand 7" />
+            </a> <p>Li Auto</p>
+         
+        </div>
+        <div className="col-6 col-md-2 brand-item">
+          <a href="/">
+            <img src="https://zgh.com/media-center/wp-content/uploads/sites/2/2021/02/5.2018-Geely-Auto-21.jpg" alt="Brand 8" />
+            </a><p>Geely</p>
+          
+        </div>
+        <div className="col-6 col-md-2 brand-item">
+          <a href="/">
+            <img src="https://guangcaicar.com/wp-content/uploads/2023/10/Aion-logo.png" alt="Brand 9" />
+            </a> <p>Aion</p>
+          
+        </div>
+        <div className="col-6 col-md-2 brand-item">
+          <a href="/">
+            <img src="https://guangcaiauto.com/wp-content/uploads/elementor/thumbs/Changan-logo-qhp368it1awabdxvs5dqs23ptclysu6amga68q1u58.png" alt="Brand 10" />
+            </a><p>Changan</p>
+         
+        </div>
+        <div className="col-6 col-md-2 brand-item">
+          <a href="/">
+            <img src="https://static.wixstatic.com/media/f2bf43_eee224b9ebca45d081fcf3b0a4d1508f~mv2.png" alt="Brand 11" />
+            </a> <p>Chery</p>
+         
+        </div>
+        <div className="col-6 col-md-2 brand-item">
+          <a href="/">
+            <img src="https://i.namu.wiki/i/dvEsuVx8W6hn1meUPbMRjBcUbf4Xq6SZUdMnMygMPvgg24SZw1r_ZHn16ZBt-bg0qAYZKzRzMR_HBGQPC5alrA.webp" alt="Brand 12" />
+            </a><p>Tank</p>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PremiumBrands;
