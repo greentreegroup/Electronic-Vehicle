b7acd6d1e76893ac90c888543b4a4c59cbad7dbd
Ibrahem <153446257+iibrahemali@users.noreply.github.com>
2024-10-21 04:12:08 -0400

    Premium Brands UI/UX

--- src/components/PremiumBrands.js
+++ src/components/PremiumBrands.js
@@ -1,84 +1,32 @@
 import React from 'react';
-import './PremiumBrands.css'; 
+import './PremiumBrands.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function PremiumBrands() {
+  const brands = [
+    { img: "https://upload.wikimedia.org/wikipedia/commons/0/0e/BYD_Auto_Logo.svg", alt: "BYD Auto" },
+    { img: "https://upload.wikimedia.org/wikipedia/en/2/21/Roewe_logo.png", alt: "Roewe" },
+    { img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/MG_Motor_2021_logo.svg/1920px-MG_Motor_2021_logo.svg.png", alt: "MG Motor" },
+    { img: "https://guangcaiauto.com/wp-content/uploads/elementor/thumbs/IM-logo-qhp2w7ktcry1mjbmenjprk72cvsg2xlwj4v22wv9i4.png", alt: "IM Motors" },
+    { img: "https://1000logos.net/wp-content/uploads/2023/09/Nio-Logo.png", alt: "Nio" },
+    { img: "https://static.wixstatic.com/media/f2bf43_a50c0080788f4b9fa5bff97c40bbdc1f~mv2.png", alt: "Brand 6" },
+    { img: "https://upload.wikimedia.org/wikipedia/commons/2/21/Li_Auto_logo.png", alt: "Li Auto" },
+    { img: "https://guangcaicar.com/wp-content/uploads/2023/10/Aion-logo.png", alt: "Aion" },
+    { img: "https://static.wixstatic.com/media/f2bf43_eee224b9ebca45d081fcf3b0a4d1508f~mv2.png", alt: "EV" },
+    { img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Geely_Auto_2023.svg/2880px-Geely_Auto_2023.svg.png", alt: "Geely" },
+  ];
+
   return (
     <div className="premium-brands-container">
-      <h2>Our Premium Brands</h2>
-      <div className="brand-logos row">
-        <div className="col-6 col-md-2 brand-item">
-          <a className="link-in-progress" href="/">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/0/0e/BYD_Auto_Logo.svg" alt="Brand 1" />
-            </a> <p>BYD</p>
-        
-        </div>
-        <div className="col-6 col-md-2 brand-item">
-          <a className="link-in-progress" href="/">
-            <img src="https://e7.pngegg.com/pngimages/510/529/png-clipart-car-logo-roewe-icons-logos-emojis-car-logos.png" alt="Brand 2" />
-            </a> <p>Roewe</p>
-         
-        </div>
-        <div className="col-6 col-md-2 brand-item">
-          <a className="link-in-progress" href="/">
-            <img src="https://i.pinimg.com/736x/58/bc/c8/58bcc81b0825551e87e289f4263c7feb.jpg" alt="Brand 3" />
-            </a> <p>MG</p>
-          
-        </div>
-        <div className="col-6 col-md-2 brand-item">
-          <a className="link-in-progress" href="/">
-            <img src="https://guangcaiauto.com/wp-content/uploads/elementor/thumbs/IM-logo-qhp2w7ktcry1mjbmenjprk72cvsg2xlwj4v22wv9i4.png" alt="Brand 4" />
-            </a>  <p>IM Motor</p>
-          
-        </div>
-        <div className="col-6 col-md-2 brand-item">
-          <a className="link-in-progress" href="/">
-            <img src="https://1000logos.net/wp-content/uploads/2023/09/Nio-Logo.png" alt="Brand 5" />
-            </a><p>NIO</p>
-        
-        </div>
-        <div className="col-6 col-md-2 brand-item">
-          <a className="link-in-progress" href="/">
-            <img src="https://static.wixstatic.com/media/f2bf43_a50c0080788f4b9fa5bff97c40bbdc1f~mv2.png" alt="Brand 6" />
-            </a> <p>GAC Motor</p>
-         
-        </div>
-        <div className="col-6 col-md-2 brand-item">
-          <a className="link-in-progress" href="/">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/2/21/Li_Auto_logo.png" alt="Brand 7" />
-            </a> <p>Li Auto</p>
-         
-        </div>
-        <div className="col-6 col-md-2 brand-item">
-          <a className="link-in-progress" href="/">
-            <img src="https://zgh.com/media-center/wp-content/uploads/sites/2/2021/02/5.2018-Geely-Auto-21.jpg" alt="Brand 8" />
-            </a><p>Geely</p>
-          
-        </div>
-        <div className="col-6 col-md-2 brand-item">
-          <a className="link-in-progress" href="/">
-            <img src="https://guangcaicar.com/wp-content/uploads/2023/10/Aion-logo.png" alt="Brand 9" />
-            </a> <p>Aion</p>
-          
-        </div>
-        <div className="col-6 col-md-2 brand-item">
-          <a className="link-in-progress" href="/">
-            <img src="https://guangcaiauto.com/wp-content/uploads/elementor/thumbs/Changan-logo-qhp368it1awabdxvs5dqs23ptclysu6amga68q1u58.png" alt="Brand 10" />
-            </a><p>Changan</p>
-         
-        </div>
-        <div className="col-6 col-md-2 brand-item">
-          <a className="link-in-progress" href="/">
-            <img src="https://static.wixstatic.com/media/f2bf43_eee224b9ebca45d081fcf3b0a4d1508f~mv2.png" alt="Brand 11" />
-            </a> <p>Chery</p>
-         
-        </div>
-        <div className="col-6 col-md-2 brand-item">
-          <a className="link-in-progress" href="/">
-            <img src="https://i.namu.wiki/i/dvEsuVx8W6hn1meUPbMRjBcUbf4Xq6SZUdMnMygMPvgg24SZw1r_ZHn16ZBt-bg0qAYZKzRzMR_HBGQPC5alrA.webp" alt="Brand 12" />
-            </a><p>Tank</p>
-          
-        </div>
+      <h2 className="section-title">Our Premium Brands</h2>
+      <div className="brand-grid">
+        {brands.map((brand, index) => (
+          <div key={index} className="brand-item">
+            <a href="/" className="link-in-progress">
+              <img src={brand.img} alt={brand.alt} />
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
